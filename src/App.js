5d9f6f4b32b99e824b6237bcdd35aa1358ac382c
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-06 12:29:09 -0600

    hashrouter mini

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import routes from './routes';
+import {Link} from 'react-router-dom'; // pulling from an already existent package 
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <Link to="/">
+         <p> Route 1 </p>
+        </Link>
+
+        <Link to="/Route2">
+         <p> Route 2 </p>
+        </Link>
+
+        <Link to="/Route3">
+         <p> Route 3 </p>
+        </Link>
+
+        { routes }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react'; // because we will be using JSX
+import {Switch, Route} from 'react-router-dom';
+import Route1 from './Route1';
+import Route2 from './Route2';
+import Route3 from './Route3';
+
+export default (
+    <Switch>
+        <Route exact path="/" component={Route1} />
+        <Route path="/Route2" component={Route2} />
+        <Route path="/Route3" component={Route3} />
+    </Switch>
+);
